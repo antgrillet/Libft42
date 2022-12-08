e652cbe3172563d823918482f4a0a52dfc175185
antonin <anto73grillet@gmail.com>
2022-12-08 13:43:36 +0100

    8/12/2022 1:43 PM

--- libft.h
+++ libft.h
@@ -4,6 +4,13 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int ft_strlen(char *c)
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
+
+
+size_t ft_strlen(char *c)
 
 #endif
