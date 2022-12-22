b5c2122f43ecf00013e4cf166da2a758937b1c27
anto73grillet <anto73grillet@gmail.com>
2022-12-22 09:49:29 +0100

    strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -12,14 +12,18 @@
 
 #include "libft.h"
 
-size_t		find_start(char const *s1, char const *set)
-{
-	
-}
-
 char *ft_strtrim(char const *s1, char const *set)
 {
+	size_t	i;
 
+	if (!s1 || !set)
+		return (0);
+	while (*s1 && ft_strchr(set, *s1))
+		s1++;
+	i = ft_strlen(s1);
+	while (i && ft_strchr(set, s1[i]))
+		i--;
+	return (ft_substr(s1, 0, i + 1));
 }
 
 /* int	main(void)
