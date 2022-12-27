bcf7376f6a7d3ab9051291c9df107ae993ead963
asmixx <95767898+asmixx@users.noreply.github.com>
2022-12-27 21:38:02 +0100

    FAIRE LA NORMAINETTE

--- ft_split.c
+++ ft_split.c
@@ -12,55 +12,63 @@
 
 #include "libft.h"
 
-size_t	count_word(char const *s, char c)
+static int	count_malloc(const char *str, char c)
 {
-	size_t	i;
-	size_t	tmp;
+	int i;
+	int marker;
 
 	i = 0;
-	tmp = 0;
-
-	while (s[i])
+	marker = 0;
+	while (*str)
 	{
-		if(s[i] == c)
-			tmp++;
-		i++;
+		if (*str != c && marker == 0)
+		{
+			marker = 1;
+			i++;
+		}
+		else if (*str == c)
+			marker = 0;
+		str++;
 	}
-	return(tmp);
+	return (i);
 }
 
-char	**split_word(const char *s, char c, char **tab)
+static char	*word_dup(const char *str, int start, int finish)
 {
-	size_t	start;
-	size_t	end;
+	char	*word;
+	int		i;
 
-	start = 0;
-	end = 0;
-	while(s[end])
-	{
-		while(s[end] != c)
-			end++;
-		if (s[end - 1] != c)
-			*tab = ft_substr(s, start , end - start);
-		start = end;
-		tab++;
-		end++;
-	}
-	*tab = "\0";
-	return(tab);
+	i = 0;
+	word = malloc((finish - start + 1) * sizeof(char));
+	while (start < finish)
+		word[i++] = str[start++];
+	word[i] = '\0';
+	return (word);
 }
-char	**ft_split(char const *s, char c)
+
+char		**ft_split(char const *s, char c)
 {
 	size_t	i;
 	size_t	j;
-	char	**tab;
+	int		index;
+	char	**split;
 
+	if (!s || !(split = malloc((count_malloc(s, c) + 1) * sizeof(char *))))
+		return (0);
 	i = 0;
-	tab = malloc(sizeof(char *) * count_word(s, c) + 1);
 	j = 0;
-	if (!s || !tab)
-		return(NULL);
-	return(split_word(s, c, tab));
-	
-
+	index = -1;
+	while (i <= ft_strlen(s))
+	{
+		if (s[i] != c && index < 0)
+			index = i;
+		else if ((s[i] == c || i == ft_strlen(s)) && index >= 0)
+		{
+			split[j++] = word_dup(s, index, i);
+			index = -1;
+		}
+		i++;
+	}
+	split[j] = 0;
+	return (split);
 }
